0e859a4883fd8791773dbdf4da128cc58bfefcfa
shyamchand <106597804+shyamchand114@users.noreply.github.com>
2022-09-01 13:43:48 +0530

    Update cyclistic_analysis_file.R

--- cyclistic_analysis_file.R
+++ cyclistic_analysis_file.R
@@ -1,39 +1,41 @@
 
-### install the packages that are needed to wrangle the data.
+# install the packages that are needed to wrangle the data.
 install.packages("tidyverse")
 install.packages("lubridate")
 install.packages("hms")
 
-
-
-
+# Load the packages 
 library(tidyverse)
 library(janitor)
 library(lubridate)
 library(hms)
 
+# Load the data that is saved in the local folder
+aug_2021 <- read.csv("C:/202108-divvy-tripdata.csv")
+sep_2021 <- read.csv("C:/202109-divvy-tripdata.csv")
+oct_2021 <- read.csv("C:/202110-divvy-tripdata.csv")
+nov_2021 <- read.csv("C:/202111-divvy-tripdata.csv")
+dec_2021 <- read.csv("C:/202112-divvy-tripdata.csv")
+jan_2022 <- read.csv("C:/202201-divvy-tripdata.csv")
+feb_2022 <- read.csv("C:/202202-divvy-tripdata.csv")
+mar_2022 <- read.csv("C:/202203-divvy-tripdata.csv")
+apr_2022 <- read.csv("C:/202204-divvy-tripdata.csv")
+may_2022 <- read.csv("C:/202205-divvy-tripdata.csv")
+jun_2022 <- read.csv("C:/202206-divvy-tripdata.csv")
+jul_2022 <- read.csv("C:/202207-divvy-tripdata.csv")
 
-aug_2021 <- read.csv("C:/Users/syamc/OneDrive/Desktop/cyclistic/files csv/202108-divvy-tripdata.csv")
-sep_2021 <- read.csv("C:/Users/syamc/OneDrive/Desktop/cyclistic/files csv/202109-divvy-tripdata.csv")
-oct_2021 <- read.csv("C:/Users/syamc/OneDrive/Desktop/cyclistic/files csv/202110-divvy-tripdata.csv")
-nov_2021 <- read.csv("C:/Users/syamc/OneDrive/Desktop/cyclistic/files csv/202111-divvy-tripdata.csv")
-dec_2021 <- read.csv("C:/Users/syamc/OneDrive/Desktop/cyclistic/files csv/202112-divvy-tripdata.csv")
-jan_2022 <- read.csv("C:/Users/syamc/OneDrive/Desktop/cyclistic/files csv/202201-divvy-tripdata.csv")
-feb_2022 <- read.csv("C:/Users/syamc/OneDrive/Desktop/cyclistic/files csv/202202-divvy-tripdata.csv")
-mar_2022 <- read.csv("C:/Users/syamc/OneDrive/Desktop/cyclistic/files csv/202203-divvy-tripdata.csv")
-apr_2022 <- read.csv("C:/Users/syamc/OneDrive/Desktop/cyclistic/files csv/202204-divvy-tripdata.csv")
-may_2022 <- read.csv("C:/Users/syamc/OneDrive/Desktop/cyclistic/files csv/202205-divvy-tripdata.csv")
-jun_2022 <- read.csv("C:/Users/syamc/OneDrive/Desktop/cyclistic/files csv/202206-divvy-tripdata.csv")
-jul_2022 <- read.csv("C:/Users/syamc/OneDrive/Desktop/cyclistic/files csv/202207-divvy-tripdata.csv")
-
-
+# compare the Data frames of all the loaded files are same or not
 compare_df_cols_same(aug_2021, sep_2021, oct_2021, nov_2021, dec_2021, jan_2022, feb_2022, mar_2022, apr_2022, may_2022, jun_2022,jul_2022,bind_method = c("bind_rows", "rbind"), verbose = TRUE)
 
+# Bind or Append all the tbales data into one table i.e., Cyclistic
 cyclistic <- rbind(aug_2021, sep_2021, oct_2021, nov_2021, dec_2021, jan_2022, feb_2022, mar_2022, apr_2022, may_2022, jun_2022,jul_2022)
 
+# Now summarize the new Data set
 summary(cyclistic)
 
+# View the cyclistic Data set
 View(cyclistic)
+
 
 str(aug_2021)
  
@@ -41,7 +43,6 @@ str(aug_2021)
 # PROCESS
 # Remove geographical indication contained in columns 9 to 12
 cyclistic <- cyclistic[ -c(9:12) ]
-
 
 # Check for duplicates
 length(unique(cyclistic$ride_id)) == nrow(cyclistic)
@@ -61,6 +62,7 @@ unique(cyclistic$member_casual)
 # Convert character to datetime
 cyclistic$started_at <- as.POSIXct(cyclistic$started_at, format = "%Y-%m-%d %H:%M:%S")
 cyclistic$ended_at <- as.POSIXct(cyclistic$ended_at, format = "%Y-%m-%d %H:%M:%S")
+
 
 #check if it was converted or not
 str(cyclistic)
@@ -83,16 +85,11 @@ cyclistic$time <- as_hms((cyclistic$started_at))
 cyclistic$hour <- as_hms(floor_date(cyclistic$started_at, unit = "hour"))
 
 
-# Order weekday
-cyclistic$weekday <- ordered(cyclistic$weekday, levels = c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"))
-
-
-# Save new data set
+# Save the new data set 
 write.csv(cyclistic, "cyclistic.csv")
 
 
 # ANALYZE
-
 ## Compare members and casual users
 aggregate(cyclistic$ride_length ~ cyclistic$member_casual, FUN = mean)
 aggregate(cyclistic$ride_length ~ cyclistic$member_casual, FUN = median)
@@ -105,8 +102,10 @@ aggregate(cyclistic$ride_length ~ cyclistic$member_casual + cyclistic$weekday, F
 # Order by weekday
 cyclistic$weekday <- ordered(cyclistic$weekday, levels=c("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"))
 
+
 # Now see Average time by Each day
 aggregate(cyclistic$ride_length ~ cyclistic$member_casual + cyclistic$weekday, FUN = mean)
+
 
 # Rides by User Type
 cyclistic %>%
